6dfaabc65088f0560b0fde88df23f6db746e354a
331535900 <331535900@qq.com>
2019-09-26 12:34:27 +0800

    add

--- Model/ffmpeg.php
+++ Model/ffmpeg.php
@@ -10,12 +10,18 @@ class ffmpeg{
         $url=self::$urlset;
         $dir=STOR_DIR.'/'.date('y').'/'.date('m').'/'.date('d').'/'.date('H_i_s');//视频存储路径
         mkdir($dir,0777,true);
+        //去除文件名的空格
+        $newurl=preg_replace('# #','',$url);
+        rename($url,$newurl);
+        $url=$newurl;
         //echo 'ffmpeg -i '.$url.' -c copy -strict -2 -bsf:v h264_mp4toannexb -f hls -hls_list_size 0 -hls_time 5 '.$dir.'/index.m3u8'; exit();
         //shell_exec('ffmpeg -i '.$url.' -c copy -strict -2 -bsf:v h264_mp4toannexb -f hls -hls_list_size 0 -hls_time 5 '.$dir.'/index.m3u8');
         //shell_exec('ffmpeg -i '.$url.' -r 5 -q:v 2 -f image2 -vframes 10 '.$dir.'/image-%d.jpeg');
         $url_name =substr($url,0, strrpos($url,'.'));//文件地址和明文件的名字链接
         $last_name=substr($url,strrpos($url,'.')+1);//后缀名字
-        //如果mp4格式
+      
+              
+        //如果不是mp4格式
         if($last_name!='mp4'){
             shell_exec('ffmpeg -i '.$url.' -y -c:v libx264 -strict -2 '.$url_name.'.mp4');//如果视频不为mp4格式，需先将视频转码为mp4，可使用如下命令进行转换
             //shell_exec('ffmpeg -y -i '.$url_name.'.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb '.$dir.'/index.ts');//将mp4格式转换为ts格式
--- run.php
+++ run.php
@@ -3,8 +3,8 @@ require_once ('/www/wwwroot/node_project/Model/Db.php');
 require_once ('/www/wwwroot/node_project/Model/ffmpeg.php');
 define('STOR_DIR','/www/wwwroot/node_project');//视频存储文件夹
 //$url=$argv['1'];//视频路径
-//$url='/www/wwwroot/node_project/14.mp4';//视频路径
-$url='/www/wwwroot/node_project/avi.avi';//视频路径
+$url='/usr/local/caddy/www/aria2/Download/919加勒比 辦公室淫亂群交盛宴極品OL女神波多野結衣.mp4';//视频路径
+//$url='/www/wwwroot/node_project/avi.avi';//视频路径
 //file_put_contents('/www/wwwroot/node_project/666.txt',$url);
 $ff=new ffmpeg($url);
 $indexm3u8url=$ff->cliceAndGetImage();//切片并且获取图片
